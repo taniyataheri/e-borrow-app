c186110b41c5d79a0a3dfe7c484b7451d991e675
Exception-th <itthikorn.peingern@gmail.com>
2025-05-02 00:35:42 +0700

    update database

--- church_property_db_ล่าสุด.sql
+++ church_property_db_ล่าสุด.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2025 at 10:15 AM
+-- Generation Time: May 01, 2025 at 07:33 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.3.20
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,6 +39,18 @@ CREATE TABLE `borrow_request` (
   `note` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `borrow_request`
+--
+
+INSERT INTO `borrow_request` (`request_id`, `member_id`, `product_id`, `quantity`, `request_date`, `due_return_date`, `receive_date`, `return_date`, `note`) VALUES
+(1, 'M004', 1, 6, '2025-05-01', '2025-05-08', '2025-05-01', NULL, 'ยืม 6ตัว'),
+(2, 'M004', 4, 2, '2025-05-01', '2025-05-08', '2025-05-01', NULL, 'asdas'),
+(3, 'M004', 6, 1, '2025-05-01', '2025-05-08', '2025-05-01', NULL, 'asdasd'),
+(4, 'M004', 1, 1, '2025-05-01', '2025-05-08', '2025-05-01', NULL, 'asdasd'),
+(5, 'M004', 4, 3, '2025-05-01', '2025-05-08', '2025-05-01', NULL, 'ฟหกฟหก'),
+(6, 'M004', 1, 3, '2025-05-01', '2025-05-08', '2025-05-01', NULL, 'asdsad');
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +65,18 @@ CREATE TABLE `borrow_request_status` (
   `canceled_by` varchar(255) DEFAULT NULL,
   `updated_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `borrow_request_status`
+--
+
+INSERT INTO `borrow_request_status` (`status_id`, `request_id`, `status_name`, `cancel_reason`, `canceled_by`, `updated_date`) VALUES
+(1, 1, 'คืนของแล้ว', NULL, NULL, NULL),
+(2, 2, 'คืนของแล้ว/มีของชำรุด', NULL, NULL, NULL),
+(3, 3, 'คืนของแล้ว/มีของชำรุด', NULL, NULL, NULL),
+(4, 4, 'คืนของแล้ว', NULL, NULL, NULL),
+(5, 5, 'คืนของแล้ว/มีของชำรุด', NULL, NULL, NULL),
+(6, 6, 'คืนของแล้ว/มีของชำรุด', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -78,7 +102,7 @@ INSERT INTO `categories` (`category_id`, `name`, `status`) VALUES
 (5, 'อุปกรณ์ตกแต่งเสริม (นักร้อง)', 1),
 (6, 'อุปกรณ์ตกแต่งเสริม (ร่ายรำ)', 1),
 (7, 'ครุภัณฑ์', 1),
-(8, 'react2', 0),
+(8, 'react2', 1),
 (9, 'ves22', 0),
 (10, 'เกษ', 0),
 (11, 'เกษ', 0);
@@ -110,8 +134,8 @@ CREATE TABLE `members` (
 --
 
 INSERT INTO `members` (`member_id`, `prefix`, `first_name`, `last_name`, `full_name`, `email`, `team`, `phone_number`, `birthday`, `username`, `password`, `role_id`, `join_date`) VALUES
-('M001', 'นาย', 'สมชาย', 'ใจดี', 'สมชาย ใจดี', 'test@example.com', 'A', '0800000000', '1995-05-01', 'testuser', '123456789', 1, '2025-04-19 22:01:30'),
-('M002', 'นางสาว', 'ส้มโอ', 'ธรรมดี', 'ส้มโอ ธรรมดี', 'user@example.com', 'B', '0891234567', '2000-08-15', 'normaluser', '123456789', 2, '2025-04-19 22:01:30'),
+('M001', 'นาย', 'สมชาย', 'ใจดี', 'สมชาย ใจดี', 'test@example.com', 'A', '0800000000', '1995-05-01', 'testuser', '123456', 1, '2025-04-19 22:01:30'),
+('M002', 'นางสาว', 'ส้มโอ', 'ธรรมดี', 'ส้มโอ ธรรมดี', 'user@example.com', 'B', '0891234567', '2000-08-15', 'normaluser', '123456', 2, '2025-04-19 22:01:30'),
 ('M003', 'นางสาว', 'Yutita', 'Kaewlom', 'Yutita Kaewlom', 'Yutita.ka@t111fac.or.th', 'H', '0653876135', '2007-04-24', 'yutitak', '123456', 2, '2025-04-25 23:16:16'),
 ('M004', 'นาย', 'itthkorn', 'peingern', 'itthkorn peingern', 'test@test.com', 'G', '0971517780', '2000-10-29', 'iomasterz', '123456', 2, '2025-04-28 11:41:43');
 
@@ -162,11 +186,11 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `name`, `color`, `quantity`, `size`, `price_per_item`, `category_id`, `status`, `image`) VALUES
-(1, 'เหลืองลูกไม้', '', 2, 'M', 350.00, 1, 'พร้อมใช้งาน', 'https://i.postimg.cc/bYV68NzV/yellow-lace-and-jersey-slit-prom-dress-2.webp'),
+(1, 'เหลืองลูกไม้', '', 26, 'M', 350.00, 1, 'พร้อมใช้งาน', 'https://i.postimg.cc/bYV68NzV/yellow-lace-and-jersey-slit-prom-dress-2.webp'),
 (2, 'เดรสโอรส', 'โอรส', 1, 'L', 400.00, 1, 'รายการนี้ถูกลบแล้ว', 'https://i.postimg.cc/K8xQDKQz/iz63bp.jpg'),
 (3, 'เดรสบานเย็น', NULL, 0, NULL, 300.00, 1, 'รายการนี้ถูกลบแล้ว', 'https://i.postimg.cc/xTYLWwmp/images.jpg'),
-(4, 'เดรสชมพูกะปิจีบรอบ', NULL, 3, NULL, 400.00, 1, 'พร้อมใช้งาน', 'https://i.postimg.cc/sgKFYCYk/th-11134207-7ras9-m1fe1h2a2zebe3.jpg'),
-(5, 'เดรสเขียวเข้ม', NULL, 6, NULL, 350.00, 1, 'พร้อมใช้งาน', 'https://i.postimg.cc/bJ74yyPt/images-1.jpg'),
+(4, 'เดรสชมพูกะปิจีบรอบ', '', 3, '', 400.00, 1, 'พร้อมใช้งาน', 'https://i.postimg.cc/sgKFYCYk/th-11134207-7ras9-m1fe1h2a2zebe3.jpg'),
+(5, 'เดรสเขียวเข้ม', NULL, 3, NULL, 350.00, 1, 'พร้อมใช้งาน', 'https://i.postimg.cc/bJ74yyPt/images-1.jpg'),
 (6, 'มินิเดรสน้ำเงิน', NULL, 4, NULL, 350.00, 1, 'พร้อมใช้งาน', 'https://i.postimg.cc/wTV4Pc1J/c7thdf.jpg'),
 (7, 'เดรสกระโปรงม่วงพาสเทล ผ้าชีฟอง', NULL, 1, NULL, 300.00, 1, 'พร้อมใช้งาน', 'https://i.postimg.cc/7h4XtfDm/th-11134207-7r990-lxaxltcs0x5b71-tn.webp'),
 (8, 'เดรสกระโปรงเขียวพาสเทล ผ้าชีฟอง', NULL, 1, NULL, 300.00, 1, 'พร้อมใช้งาน', 'https://i.postimg.cc/9XBBFXcH/78.jpg'),
@@ -377,14 +401,32 @@ CREATE TABLE `return_detail` (
   `returned_good` int(11) DEFAULT NULL,
   `returned_damaged` int(11) DEFAULT NULL,
   `returned_lost` int(11) DEFAULT NULL,
-  `repaired_quantity` int(11) DEFAULT NULL,
   `return_date` date DEFAULT NULL,
   `fine_amount` text DEFAULT NULL,
   `received_by` varchar(255) DEFAULT NULL,
   `returned_by` varchar(255) DEFAULT NULL,
   `note` text DEFAULT NULL,
-  `repair_note` text DEFAULT NULL
+  `repair_note` text DEFAULT NULL,
+  `repaired_quantity` int(11) NOT NULL DEFAULT 0,
+  `repair_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `return_detail`
+--
+
+INSERT INTO `return_detail` (`return_id`, `request_id`, `returned_good`, `returned_damaged`, `returned_lost`, `return_date`, `fine_amount`, `received_by`, `returned_by`, `note`, `repair_note`, `repaired_quantity`, `repair_date`) VALUES
+(1, 1, 1, 0, 0, '2025-05-01', 'ไม่มีค่าปรับ', 'M004', 'M001', '', NULL, 0, NULL),
+(2, 1, 1, 0, 0, '2025-05-01', 'ไม่มีค่าปรับ', 'M004', 'M001', '', NULL, 0, NULL),
+(3, 1, 1, 0, 0, '2025-05-01', 'ไม่มีค่าปรับ', 'M004', 'M001', '', NULL, 0, NULL),
+(4, 1, 1, 0, 0, '2025-05-01', 'ไม่มีค่าปรับ', 'M004', 'M001', '', NULL, 0, NULL),
+(5, 1, 0, 0, 1, '2025-05-01', 'ไม่มีค่าปรับ', 'M004', 'M001', 'ของมีรอยขีดข่วนหรือแตกหัก', 'ฟหกฟห', 6, '2025-05-02'),
+(6, 1, 0, 0, 1, '2025-05-01', 'ค่าของสูญหาย 350 บาท', 'M004', 'M001', '', NULL, 0, NULL),
+(7, 2, 0, 0, 1, '2025-05-01', 'ค่าของสูญหาย 400 บาท', 'M004', 'M001', '', 'asdasd', 1, NULL),
+(8, 3, 0, 0, 0, '2025-05-01', 'ไม่มีค่าปรับ', 'M004', 'M001', '', 'ซ่อมแล้ว', 1, NULL),
+(9, 4, 0, 0, 1, '2025-05-01', 'ค่าของสูญหาย 350 บาท', 'M004', 'M001', 'หาของไม่พบ ต้องจ่ายเต็มราคา', NULL, 0, NULL),
+(10, 5, 0, 1, 1, '2025-05-01', 'ไม่มีค่าปรับ', 'M004', 'M001', '', 'ซ่อมเสดตัวเดียว', 1, NULL),
+(11, 6, 0, 0, 0, '2025-05-01', 'ไม่มีค่าปรับ', 'M004', 'M001', 'ของมีรอยขีดข่วนหรือแตกหัก', 'asdas', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -508,13 +550,13 @@ ALTER TABLE `sizes`
 -- AUTO_INCREMENT for table `borrow_request`
 --
 ALTER TABLE `borrow_request`
-  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `borrow_request_status`
 --
 ALTER TABLE `borrow_request_status`
-  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -538,7 +580,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `return_detail`
 --
 ALTER TABLE `return_detail`
-  MODIFY `return_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `return_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `roles`
