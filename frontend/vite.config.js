dbcc5ce8db6910cb2f3e788348d998dc1f7c636c
taniyataheri <taniya.t@kkumail.com>
2025-05-21 23:46:21 +0700

    Edit vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 80,
+  },
   server: {
     host: true,
+    port: 3000,
     allowedHosts: [
       '4139-2001-fb1-11e-8ab5-dd5a-ae6e-172-f71a.ngrok-free.app'
     ],
